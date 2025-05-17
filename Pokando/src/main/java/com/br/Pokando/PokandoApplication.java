6a62d11ea7a6d3e43e14e28104e806cbbe8d3c5f
Felipe Farias <lipee1223@gmail.com>
2025-05-17 14:24:53 -0300

    Pasta criada

---
+++ Pokando/src/main/java/com/br/Pokando/PokandoApplication.java
@@ -0,0 +1,13 @@
+package com.br.Pokando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokandoApplication.class, args);
+	}
+
+}
---
+++ Pokando/src/test/java/com/br/Pokando/PokandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Pokando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
