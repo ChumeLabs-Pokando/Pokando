779faef70d04297393aaf8a1ef55e24d689a890c
TioPotat0 <lipee1223@gmail.com>
2025-10-17 22:38:24 -0400

    TagCategoria e Cargo REMOVIDOS

--- Pokando/Pokando/src/main/java/com/br/Pokando/repository/CargoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.Pokando.repository;
-
-import com.br.Pokando.model.Cargo;
-import com.br.Pokando.model.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CargoRepository extends JpaRepository<Cargo, Long> {
-}
--- Pokando/Pokando/src/main/java/com/br/Pokando/repository/TagCategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.Pokando.repository;
-
-import com.br.Pokando.model.Endereco_geografico;
-import com.br.Pokando.model.TagCategoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagCategoriaRepository  extends JpaRepository<TagCategoria, Long> {
-}
