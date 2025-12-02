43a1cca76fe67ce505c8f4d61306ab05bcb0d38e
TioPotat0 <lipee1223@gmail.com>
2025-12-02 19:38:32 -0300

    adicionado pesquisa psql do nome do evento

--- Pokando/src/main/java/com/br/Pokando/Service/EventoService.java
+++ Pokando/src/main/java/com/br/Pokando/Service/EventoService.java
@@ -9,18 +9,22 @@ import com.br.Pokando.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class EventoService extends ServiceAdapter<Evento, Long, EventoResponse, EventoRequest, EventoRequestUpdate> {
 
     private final ClienteRepository clienteRepository;
     private final IngressoRepository ingressoRepository;
+    private final EventoRepository eventoRepository;
 
 
 
-    public EventoService(UserAcessoRepository userAcessoRepository, EventoRepository repository, EventoMapper mapper, ClienteRepository clienteRepository, IngressoRepository ingressoRepository) {
+    public EventoService(EventoRepository repository, EventoMapper mapper, ClienteRepository clienteRepository, IngressoRepository ingressoRepository, EventoRepository eventoRepository) {
         super(repository,mapper);
         this.clienteRepository = clienteRepository;
         this.ingressoRepository = ingressoRepository;
+        this.eventoRepository = eventoRepository;
     }
 
     @Override
@@ -30,6 +34,13 @@ public class EventoService extends ServiceAdapter<Evento, Long, EventoResponse, 
         var saved = repository.save(entity);
         return repository.findById(saved.getId())
                 .orElseThrow(() -> new RuntimeException("Erro ao buscar Evento salvo"));
+    }
+
+    public List<EventoResponse> buscarPorNome(String nome) {
+        var lista = eventoRepository.findByNomeContainingIgnoreCase(nome);
+        return lista.stream()
+                .map(mapper::toDto)
+                .toList();
     }
 
 }
--- Pokando/src/main/java/com/br/Pokando/controller/EventoController.java
+++ Pokando/src/main/java/com/br/Pokando/controller/EventoController.java
@@ -1,15 +1,22 @@
 package com.br.Pokando.controller;
 
 import com.br.Pokando.Mapper.EventoMapper;
+import com.br.Pokando.Service.EventoService;
 import com.br.Pokando.Service.IService;
+import com.br.Pokando.dto.ApiDataResponse;
 import com.br.Pokando.dto.EventoRequest;
 import com.br.Pokando.dto.EventoRequestUpdate;
 import com.br.Pokando.dto.EventoResponse;
 
 import com.br.Pokando.model.Evento;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/evento")
@@ -17,15 +24,33 @@ public class EventoController
         extends CRUDDefaultControllerAdapter<Evento, Long, EventoResponse, EventoRequest, EventoRequestUpdate> {
 
     private final EventoMapper eventoMapper;
+    private final EventoService eventoService;
 
     @Autowired
     public EventoController(
             IService<Evento, Long, EventoResponse, EventoRequest, EventoRequestUpdate> service,
-            EventoMapper eventoMapper
+            EventoMapper eventoMapper, EventoService eventoService
     ) {
         super(service, eventoMapper);
         this.eventoMapper = eventoMapper;
+        this.eventoService = eventoService;
     }
 
+    @GetMapping("/pesquisar")
+    public ResponseEntity<ApiDataResponse<List<EventoResponse>>> pesquisar(@RequestParam String nome) {
+
+        var response = eventoService.buscarPorNome(nome);
+
+        return ResponseEntity.ok(
+                new ApiDataResponse<>(
+                        "Pesquisa realizada com sucesso",
+                        200,
+                        "OK",
+                        response
+                )
+        );
+
+
+    }
 
 }
